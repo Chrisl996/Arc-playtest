fd63081831ba21aa0e7ef826176018b1f17773f3
Chrisl996 <chris.j.lane996@gmail.com>
2026-09-01 17:21:36 +0100

    Publish latest Arc camera update

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788278675|6617550';
+const CACHE_VERSION = '1788279588|5827103';
 /** @type {string} */
 const CACHE_PREFIX = 'Arc-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
