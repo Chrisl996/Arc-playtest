ee89f878e48d4efefb814a0a0a939a42803fc366
Chrisl996 <chris.j.lane996@gmail.com>
2026-09-02 17:44:40 +0100

    Publish distance-based fuel playtest build

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788346714|6548852';
+const CACHE_VERSION = '1788366576|5787534';
 /** @type {string} */
 const CACHE_PREFIX = 'Arc-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
