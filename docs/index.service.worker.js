fdd67a02623ab65ce7dc66e066c6fd8aa7247561
Chrisl996 <chris.j.lane996@gmail.com>
2026-09-02 11:37:43 +0100

    Publish Arc Level 14 playtest

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788279588|5827103';
+const CACHE_VERSION = '1788345425|6707700';
 /** @type {string} */
 const CACHE_PREFIX = 'Arc-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
