b681051086cfac6e8a398795c118929df015df9d
Chrisl996 <chris.j.lane996@gmail.com>
2026-09-08 17:22:15 +0100

    Publish Arc Web playtest build

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788882610|6244586';
+const CACHE_VERSION = '1788884504|6317707';
 /** @type {string} */
 const CACHE_PREFIX = 'Arc-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
